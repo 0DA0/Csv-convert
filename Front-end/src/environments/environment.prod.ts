9eb2d9b7bde8a52672e87e7e775f478609314136
0DA0 <arasdogukan25@gmail.com>
2026-01-13 15:11:45 +0300

    Update environment.prod.ts

--- Front-end/src/environments/environment.prod.ts
+++ Front-end/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://timetracker-api.onrender.com/api'  // Backend Render URL'inizi buraya yazın
+  apiUrl: 'https://csv-convert.onrender.com/api'  // Backend Render URL'inizi buraya yazın
 };
